60b18a6f4b4a0230e77819ac50831f9f1df5d9b8
shareef-afzal <afzalshareef1005@gmail.com>
2025-08-04 20:13:52 +0530

    changes in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-title"> <a href="/">CODIFIE</a></div>
         <div className="footer-socials">
-          <a target='_blank' href="https://www.linkedin.com/in/afzal-shareef-aa516a2a9/" >LinkedIn</a>
-          <a target='_blank' href="mailto:afzalshareef1005@gmail.com" >GMail</a>
+          <a className='footer-links' target='_blank' href="https://www.linkedin.com/in/afzal-shareef-aa516a2a9/" >LinkedIn</a>
+          <a className='footer-links' target='_blank' href="mailto:afzalshareef1005@gmail.com" >GMail</a>
         </div>
       </div>
       <div className="footer-bottom">
